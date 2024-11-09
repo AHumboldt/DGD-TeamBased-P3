bf21371d5ed1921177b92395eb4fa6dde3f6b05d
AHumboldt <alex.mezalopez@my.liu.edu>
2024-11-09 15:58:38 -0500

    Small Movement Update

--- DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/PlayerMovement.cs
+++ DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/PlayerMovement.cs
@@ -6,24 +6,40 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private GameObject Tile;
+    public bool Legal = false;
     
     // Start is called before the first frame update
     void Start()
     {
 
         Tile = GameObject.FindWithTag("Tile");
+        
+    }
+
+
+    void Update()
+    {
+
+        if (Legal == true)
+        {
+
+            if (Input.GetKeyDown(KeyCode.W)) transform.position += new Vector3(0, 1, 0);
+
+        }
+
     }
 
     void OnTriggerEnter2D(Collider2D Floor)
     {
 
         if (Floor.tag == "Tile") Debug.Log("Ooh... They touched...");
+        Legal = true;
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D Floor)
     {
-        
+        Legal = false;
     }
+
 }
