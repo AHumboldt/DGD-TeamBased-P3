413505ed85198ef9ce54b4a53f9b148137207dca
AHumboldt <alex.mezalopez@my.liu.edu>
2024-11-09 22:22:37 -0500

    This Will Be Movement in 2014
    90% there

--- DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/PlayerMovement.cs
+++ DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/PlayerMovement.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private GameObject Tile;
-    public bool Legal = false;
     
-    // Start is called before the first frame update
+    public bool ForwardPossible;
+    public bool LeftPossible;
+    public bool RightPossible;
+    
     void Start()
     {
 
@@ -20,26 +22,84 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-        if (Legal == true)
-        {
+ 
 
-            if (Input.GetKeyDown(KeyCode.W)) transform.position += new Vector3(0, 1, 0);
+            if (Input.GetKeyDown(KeyCode.W) && ForwardPossible == true) transform.position += new Vector3(0, 1, 0);
+            if (Input.GetKeyDown(KeyCode.A) && LeftPossible == true)
+            {
+                
+                transform.position += new Vector3(-1, 0, 0);
+                transform.Rotate(0, 0, 90);
 
-        }
+            }
+
+            if (Input.GetKeyDown(KeyCode.D) && RightPossible == true)
+            {
+                
+                transform.position += new Vector3(1, 0, 0);
+                transform.Rotate(0, 0, -90);
+                
+            }
+        
 
     }
 
     void OnTriggerEnter2D(Collider2D Floor)
     {
 
-        if (Floor.tag == "Tile") Debug.Log("Ooh... They touched...");
-        Legal = true;
+        if (Floor.tag == "TileF")
+        {
+            ForwardPossible = true;
+            LeftPossible = false;
+            RightPossible = false;
+        }
+        if (Floor.tag == "TileL")
+        {
+            ForwardPossible = false;
+            LeftPossible = true;
+            RightPossible = false;
+        }
 
+        if (Floor.tag == "TileR")
+        {
+            ForwardPossible = false;
+            LeftPossible = false;
+            RightPossible = true;
+        }
+        
+        if (Floor.tag == "TileFL")
+        {
+            ForwardPossible = true;
+            LeftPossible = true;
+            RightPossible = false;
+        }
+        
+        if (Floor.tag == "TileFR")
+        {
+            ForwardPossible = true;
+            LeftPossible = false;
+            RightPossible = true;
+        }
+        
+        if (Floor.tag == "TileLR")
+        {
+            ForwardPossible = false;
+            LeftPossible = true;
+            RightPossible = true;
+        }
+        
+        if (Floor.tag == "TileFLR")
+        {
+            ForwardPossible = true;
+            LeftPossible = true;
+            RightPossible = true;
+        }
+        
     }
 
     private void OnTriggerExit2D(Collider2D Floor)
     {
-        Legal = false;
+        //Legal = false;
     }
 
 }
