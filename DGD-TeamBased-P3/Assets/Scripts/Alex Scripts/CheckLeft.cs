eafe64afe77c675f557e52499bcddbb23b29739c
AHumboldt <alex.mezalopez@my.liu.edu>
2024-11-11 00:49:41 -0500

    This is a lot more complicated than I thought it would be
    Me asf

---
+++ DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/CheckLeft.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckLeft : MonoBehaviour
+{
+
+    private GameObject Tile;
+    public bool Detect = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Tile = GameObject.FindWithTag("Tile");
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D Floor)
+    {
+
+        if (Floor.tag == "Tile") Detect = true;
+
+    }
+    
+    private void OnTriggerExit2D(Collider2D Floor)
+    {
+
+        Detect = false;
+
+    }
+    
+}
--- DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/PlayerMovement.cs
+++ DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/PlayerMovement.cs
@@ -6,15 +6,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private GameObject Tile;
+
+    private GameObject ForwardDetection;
+    private GameObject LeftDetection;
+    private GameObject RightDetection;
     
     public bool ForwardPossible;
     public bool LeftPossible;
     public bool RightPossible;
+
+    public bool FacingUp = false;
+    public bool FacingDown = false;
+    public bool FacingLeft = false;
+    public bool FacingRight = false;
+
+    private CheckForward FCheck;
+    private CheckLeft LCheck;
+    private CheckRight RCheck;
+    
+    //To do List:
+    //
+    //- Input in Update, respond in FixedUpdate
     
     void Start()
     {
-
+        
         Tile = GameObject.FindWithTag("Tile");
+
+        FCheck = GameObject.FindWithTag("FTCheck").GetComponent<CheckForward>();
+        LCheck = GameObject.FindWithTag("LTCheck").GetComponent<CheckLeft>();
+        RCheck = GameObject.FindWithTag("RTCheck").GetComponent<CheckRight>();
         
     }
 
@@ -22,10 +43,55 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
- 
+        if (FCheck.Detect == true) ForwardPossible = true;
+        else ForwardPossible = false;
+        
+        if (LCheck.Detect == true) LeftPossible = true;
+        else LeftPossible = false;
+        
+        if (RCheck.Detect == true) RightPossible = true;
+        else RightPossible = false;
+        
+        if (this.transform.rotation.eulerAngles.z == 0) //up
+        {
+            FacingUp = true;
+            FacingDown = false;
+            FacingLeft = false;
+            FacingRight = false;
+        }
+        else if (this.transform.rotation.eulerAngles.z == 180) //Down
+        {
+            FacingUp = false;
+            FacingDown = true;
+            FacingLeft = false;
+            FacingRight = false;
+        }
+        else if (this.transform.rotation.eulerAngles.z == 90) //left
+        {
+            FacingUp = false;
+            FacingDown = false;
+            FacingLeft = true;
+            FacingRight = false;
+        }
+        else if (this.transform.rotation.eulerAngles.z == -90) //right
+        {
+            FacingUp = false;
+            FacingDown = false;
+            FacingLeft = false;
+            FacingRight = true;
+        }
 
-            if (Input.GetKeyDown(KeyCode.W) && ForwardPossible == true) transform.position += new Vector3(0, 1, 0);
-            if (Input.GetKeyDown(KeyCode.A) && LeftPossible == true)
+        if (Input.GetKeyDown(KeyCode.W) && ForwardPossible == true)
+        {
+            
+            if(FacingUp == true)transform.position += new Vector3(0, 1, 0);
+            if(FacingDown == true)transform.position += new Vector3(0, -1, 0);
+            if(FacingLeft == true)transform.position += new Vector3(-1, 0, 0);
+            if(FacingRight == true)transform.position += new Vector3(1, 0, 0);
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) && LeftPossible == true)
             {
                 
                 transform.position += new Vector3(-1, 0, 0);
@@ -47,7 +113,7 @@ public class PlayerMovement : MonoBehaviour
     void OnTriggerEnter2D(Collider2D Floor)
     {
 
-        if (Floor.tag == "TileF")
+        /*if (Floor.tag == "TileF")
         {
             ForwardPossible = true;
             LeftPossible = false;
@@ -66,35 +132,35 @@ public class PlayerMovement : MonoBehaviour
             LeftPossible = false;
             RightPossible = true;
         }
-        
+
         if (Floor.tag == "TileFL")
         {
             ForwardPossible = true;
             LeftPossible = true;
             RightPossible = false;
         }
-        
+
         if (Floor.tag == "TileFR")
         {
             ForwardPossible = true;
             LeftPossible = false;
             RightPossible = true;
         }
-        
+
         if (Floor.tag == "TileLR")
         {
             ForwardPossible = false;
             LeftPossible = true;
             RightPossible = true;
         }
-        
+
         if (Floor.tag == "TileFLR")
         {
             ForwardPossible = true;
             LeftPossible = true;
             RightPossible = true;
-        }
-        
+        }*/
+
     }
 
     private void OnTriggerExit2D(Collider2D Floor)
