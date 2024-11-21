3bb3fb758e0fc345aeed162ba0ed8890b3f88179
AHumboldt <alex.mezalopez@my.liu.edu>
2024-11-21 01:08:20 -0500

    Roam works
    Yeah

--- DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/Enemy/EnemyMovement.cs
+++ DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/Enemy/EnemyMovement.cs
@@ -24,7 +24,8 @@ public class EnemyMovement : MonoBehaviour
 
     public bool ReCalc = true; 
     public int Decider = 999;
-    public float Timer = 3f;
+    public string Decision;
+    public float Timer = 1.5f;
     
     void Start()
     {
@@ -40,6 +41,8 @@ public class EnemyMovement : MonoBehaviour
     void Update()
     {
 
+        Timer -= Time.deltaTime;
+        
         if (this.transform.rotation.eulerAngles.z == 0 || this.transform.rotation.eulerAngles.z == 360 || this.transform.rotation.eulerAngles.z == -360) //up
         {
             
@@ -79,57 +82,55 @@ public class EnemyMovement : MonoBehaviour
 
 
 
-
-
-        if (FCheck.Detect == true)
-        {
-            
-            ForwardPossible = true;
-            Available.Add("Forward");
-            
-        }
-
-        if (FCheck.Detect == false) ForwardPossible = false;
-        
-        if (BCheck.Detect == true)
-        {
-            
-            BackwardPossible = true;
-            Available.Add("Backward");
-            
-        }
-        if (BCheck.Detect == false) BackwardPossible = false;
-        
-        if (LCheck.Detect == true)
-        {
-            
-            LeftPossible = true;
-            Available.Add("Left");
-            
-        }
-        if (LCheck.Detect == false)
-        
-        if (RCheck.Detect == true)
-        {
-            
-            RightPossible = true;
-            Available.Add("Right");
-            
-        }
-        if (RCheck.Detect == false) RightPossible = false;
-
-
-
         if (ReCalc == true)
         {
             
-            for (int i = 0; i < Available.Count; i++)
+            Available.Clear();
+            
+            if (FCheck.Detect == true)
             {
             
-                Debug.Log(Available[i]);
-                ReCalc = false;
-
+                ForwardPossible = true;
+                Available.Add("Forward");
+            
             }
+
+            if (FCheck.Detect == false) ForwardPossible = false;
+        
+            if (BCheck.Detect == true)
+            {
+            
+                BackwardPossible = true;
+                Available.Add("Backward");
+            
+            }
+            if (BCheck.Detect == false) BackwardPossible = false;
+        
+            if (LCheck.Detect == true)
+            {
+            
+                LeftPossible = true;
+                Available.Add("Left");
+            
+            }
+            if (LCheck.Detect == false)
+        
+                if (RCheck.Detect == true)
+                {
+            
+                    RightPossible = true;
+                    Available.Add("Right");
+            
+                }
+            if (RCheck.Detect == false) RightPossible = false;
+            
+            for (int i = 0; i < Available.Count; i++) Debug.Log(Available[i]);
+            
+            Decider = Random.Range(0, Available.Count);
+            Decision = Available[Decider];
+            Debug.Log(Decider);
+            
+            ReCalc = false;
             
         }
 
@@ -137,7 +138,7 @@ public class EnemyMovement : MonoBehaviour
 
 
 
-        if (Input.GetKeyDown(KeyCode.W) && ForwardPossible == true)
+        if (Decision == "Forward")
         {
 
             if(FacingUp == true)transform.position += new Vector3(0, 1, 0);
@@ -145,31 +146,37 @@ public class EnemyMovement : MonoBehaviour
             if(FacingLeft == true)transform.position += new Vector3(-1, 0, 0);
             if(FacingRight == true)transform.position += new Vector3(1, 0, 0);
 
+            Decision = "";
+
         }
 
-        if (Input.GetKeyDown(KeyCode.S) && BackwardPossible == true)
+        if (Decision == "Backward")
         {
             
             if(FacingUp == true)transform.position += new Vector3(0, -1, 0);
             if(FacingDown == true)transform.position += new Vector3(0, 1, 0);
             if(FacingLeft == true)transform.position += new Vector3(1, 0, 0);
             if(FacingRight == true)transform.position += new Vector3(-1, 0, 0);
-            transform.Rotate(0, 0, 180);
+            //transform.Rotate(0, 0, 180);
+            
+            Decision = "";
             
         }
 
-        if (Input.GetKeyDown(KeyCode.A) && LeftPossible == true)
+        if (Decision == "Left")
         {
 
             if(FacingUp == true)transform.position += new Vector3(-1, 0, 0);
             if(FacingDown == true)transform.position += new Vector3(1, 0, 0);
             if(FacingLeft == true)transform.position += new Vector3(0, -1, 0);
             if(FacingRight == true)transform.position += new Vector3(0, 1, 0);
-            //transform.Rotate(0, 0, 90);
+            transform.Rotate(0, 0, 90);
+            
+            Decision = "";
 
         }
 
-        if (Input.GetKeyDown(KeyCode.D) && RightPossible == true) 
+        if (Decision == "Right") 
         {
 
             if(FacingUp == true)transform.position += new Vector3(1, 0, 0);
@@ -177,22 +184,15 @@ public class EnemyMovement : MonoBehaviour
             if(FacingLeft == true)transform.position += new Vector3(0, 1, 0);
             if(FacingRight == true)transform.position += new Vector3(0, -1, 0);
             transform.Rotate(0, 0, -90);
+            
+            Decision = "";
 
         }
         
-        Timer -= Time.deltaTime;
-        
-        if (Timer >= 3)
+        if (Timer <= 0)
         {
             
-            Decider = Random.Range(0, Available.Count);
-            Debug.Log(Decider);
-
-        }
-        else if (Timer <= 0)
-        {
-            
-            Timer = 3;
+            Timer = 1.5f;
             ReCalc = true;
             
         }
