bc941ac1244de9810139d93165da85e013f0c618
AHumboldt <alex.mezalopez@my.liu.edu>
2024-11-21 19:17:14 -0500

    Small Update 2 again
    Inane bullshit

--- DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/TileArtDisplay.cs
+++ DGD-TeamBased-P3/Assets/Scripts/Alex Scripts/TileArtDisplay.cs
@@ -7,6 +7,11 @@ public class TileArtDisplay : MonoBehaviour
     
     public List<IconInfo> IconsRaw;
     
+    //Have room detectors ON the player Check Up/Right/Left/Down
+    //Depending on the direction that checks, it shows the forward, backward, left, or right version of said room
+    //To do that: the possible routes to take will be in a list
+    //
+    
     [System.Serializable]
     public class IconInfo
     {
